fb23f1ee35c44648c20b459cf4fc574ecdcd6e1f
Shay Zeevi <shay.z@homepls.co.il>
2020-02-10 18:32:59 +0200

    gulp for watching scss and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,46 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
-const minify = require('gulp-minify');
+const {watch} = require('gulp');
+const babel = require('gulp-babel'); // Use for JS
+const minify = require('gulp-minify'); // Use for JS
+const sourcemaps = require('gulp-sourcemaps'); // Use for CSS
+const sass = require('gulp-sass'); // Use for CSS
+sass.compiler = require('node-sass'); // Use for CSS
+const browsersync = require("browser-sync").create();
 
-gulp.task('js', (cb) => gulp.src('src/theme.js')
-    .pipe(babel({
-        presets: ['@babel/env']
-    }))
-    .pipe(minify())
-    .pipe(gulp.dest('dist')));
 
-gulp.task('default', function () {
-    console.log('Hello');
-});
+function jsTask() {
+    return gulp.src('./src/js/theme.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(minify())
+        .pipe(gulp.dest('dist'));
+}
+
+function scssTask() {
+    return gulp.src('./src/css/theme.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        // .pipe(sass().on('error', sass.logError))
+        // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist'));
+}
+
+function watchFiles() {
+    gulp.watch(['./src/**/*'], gulp.series(scssTask, jsTask));
+}
+
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 3000
+    });
+}
+
+const watcher = gulp.parallel(watchFiles, browserSync);
+const build = gulp.series(gulp.parallel(watcher));
+
+exports.default = build;
