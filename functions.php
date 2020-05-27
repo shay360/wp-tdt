a5e0c8f25b9cdf35791b7fb7dfe4b80872fb1ffe
Shay Zeevi <shay.z@homepls.co.il>
2020-05-27 12:40:09 +0300

    cleaning project

--- functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Include basic theme setup
-require_once __DIR__ . '/includes/theme-setup.php';
-// Include importer for custom post types
-require_once __DIR__ . '/post-types/theme-post-types.php';
