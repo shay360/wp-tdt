5c3ed4a5ab2d8f68d0039d7e23e13ea12e9b0def
Shay Zeevi <shay.z@homepls.co.il>
2021-05-06 23:44:45 +0300

    trying action with yarn (#34)
    * trying action with yarn
    
    * Prettified Code!

---
+++ tests/jsonStringTools.test.js
@@ -0,0 +1,6 @@
+const { removeLastCommma } = require("../cli/models/json-string-tools");
+
+test("removeLastCommma", () => {
+  const result = removeLastCommma("comma removed,");
+  expect(result).toBe("comma removed");
+});
