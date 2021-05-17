fa13f4a74baedbfd4da255bdf129a9d1760a5ca7
Shay Zeevi <shay.z@homepls.co.il>
2021-05-17 20:33:08 +0300

    working on ounit tests

--- tests/jsonStringTools.test.js
+++ tests/jsonStringTools.test.js
@@ -1,6 +1,6 @@
 const { removeLastCommma } = require('../cli/models/json-string-tools');
 
-test('removeLastCommma', () => {
-	const result = removeLastCommma('comma removed,');
-	expect(result).toBe('comma removed');
+test('Remove Last Comma From String', () => {
+    const result = removeLastCommma('comma removed,');
+    expect(result).toBe('comma removed');
 });
