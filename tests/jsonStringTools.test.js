901d8b08abbc871ec30e18ce04bab406cb1d046f
Shay Zeevi <shay.z@homepls.co.il>
2021-05-17 20:40:07 +0300

    code styling

--- tests/jsonStringTools.test.js
+++ tests/jsonStringTools.test.js
@@ -1,6 +1,6 @@
 const { removeLastCommma } = require('../cli/models/json-string-tools');
 
 test('Remove Last Comma From String', () => {
-    const result = removeLastCommma('comma removed,');
-    expect(result).toBe('comma removed');
+	const result = removeLastCommma('comma removed,');
+	expect(result).toBe('comma removed');
 });
